ffe24ab548f5c2ee806b1fc029f850328e05500c
Eduardo Ribeiro da Silva <filosofisto@users.noreply.github.com>
2021-03-20 10:16:55 +0100

    First Material

---
+++ first_material/android/app/src/main/kotlin/com/noumenon/first_material/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.noumenon.first_material
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ first_material/lib/ui/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.deepOrange,
+      child: Center(
+          child: Text(
+            "Hello Material",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 24.3,
+                fontStyle: FontStyle.italic
+            ),
+          )),
+    );
+  }
+}
