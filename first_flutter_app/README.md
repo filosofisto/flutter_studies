eab24af262b96f5974cacc5b2a2528d03514fdb0
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-09-05 11:27:50 +0200

    first_flutter_app

--- first_flutter_app/README.md
+++ first_flutter_app/README.md
@@ -1,6 +1,6 @@
 # first_flutter_app
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Home());
+void main() {
+  runApp(Home());
+}
 
 class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Hello Flutter", textDirection: TextDirection.ltr,),
-    );
+        child: Text(
+      "Hello Flutter",
+      textDirection: TextDirection.ltr,
+    ));
   }
 }
-
