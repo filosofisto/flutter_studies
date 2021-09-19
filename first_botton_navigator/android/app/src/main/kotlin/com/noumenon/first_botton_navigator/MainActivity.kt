27aa6a5b5438ea5a4cf993f9c69150b49487a1df
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-09-19 17:05:37 +0200

    Remove first_botton_navigator project

--- first_botton_navigator/android/app/src/main/kotlin/com/noumenon/first_botton_navigator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.noumenon.first_botton_navigator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- first_botton_navigator/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:first_botton_navigator/ui/scaffold.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(new MaterialApp(
-  home: ScaffoldExample(),
-));
-
-
